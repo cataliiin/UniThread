430ca8901964fac5389aa3c2206e4a17e7636f3a
cataliiin <cataliiin9@gmail.com>
2026-04-26 14:25:05 +0300

    refactor(backend): changed variable settings to config to match in security.py

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -2,7 +2,7 @@ from passlib.context import CryptContext
 from jose import JWTError, jwt
 from datetime import datetime, timedelta, timezone
 from app.schemas.auth import TokenData
-from app.core.config import config as settings
+from app.core.config import config
 
 pwd_context = CryptContext(schemes=["argon2", "bcrypt"], default="argon2", deprecated="auto")
 
@@ -16,15 +16,15 @@ def create_access_token(token_data: TokenData, expires_delta: timedelta | None =
     if expires_delta:
         expire = datetime.now(timezone.utc) + expires_delta
     else:
-        expire = datetime.now(timezone.utc) + timedelta(minutes=settings.JWT_ACCESS_TOKEN_EXPIRE_MINUTES)
+        expire = datetime.now(timezone.utc) + timedelta(minutes=config.JWT_ACCESS_TOKEN_EXPIRE_MINUTES)
     
     to_encode = token_data.model_dump()
     to_encode["exp"] = expire
-    return jwt.encode(to_encode, settings.JWT_SECRET_KEY, algorithm=settings.JWT_ALGORITHM)
+    return jwt.encode(to_encode, config.JWT_SECRET_KEY, algorithm=config.JWT_ALGORITHM)
 
 def verify_token(token: str) -> TokenData | None:
     try:
-        payload_dict = jwt.decode(token, settings.JWT_SECRET_KEY, algorithms=[settings.JWT_ALGORITHM])
+        payload_dict = jwt.decode(token, config.JWT_SECRET_KEY, algorithms=[config.JWT_ALGORITHM])
         return TokenData(**payload_dict)
     except JWTError:
         return None
