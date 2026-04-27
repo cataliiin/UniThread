72385a0127b27b6f1477b33cef788d7025539ca6
SebiSomu <sebigsomu@gmail.com>
2026-04-27 20:56:24 +0300

    feat (frontend): added sidebar to layout

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<section class="p-8">
+	<h1 class="text-2xl font-bold text-slate-800">Main Page</h1>
+	<p class="text-slate-500">Welcome to UniThread! The main content of the page
+    will appear here soon.</p>
+</section>
