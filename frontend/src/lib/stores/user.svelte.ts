9c7fb8be4b5bef277c4af026d692676c3d364734
Rolland Mămăligoi <rmamaligoi@gmail.com>
2026-04-28 22:03:56 +0300

    refactor(frontend):register page and user store
    added register form and used it on register route page
    added local storage functionality on user store

--- frontend/src/lib/stores/user.svelte.ts
+++ frontend/src/lib/stores/user.svelte.ts
@@ -1,12 +1,45 @@
 class UserState {
-	name = $state('George-Sebastian Somu');
-	username = $state('sebig');
-	email = $state('george.somu@student.unitbv.ro');
-	university = $state('Transilvania University of Brașov');
-	memberSince = $state('April 2026');
-	avatarInitials = $state('GS');
+	name = $state('');
+	username = $state('');
+	email = $state('');
+	university = $state('');
+	memberSince = $state('');
+	avatarInitials = $state('');
 	avatarUrl = $state<string | null>(null);
 	avatarSource = $derived(this.avatarUrl);
+	isAuthenticated = $state(false);
+
+	async checkUsername(username: string): Promise<boolean> {
+		// Simulate API call to check username availability
+		await new Promise((resolve) => setTimeout(resolve, 500));
+
+		if (localStorage.getItem(username) === username) {
+			return false;
+		}
+		return true;
+	}
+
+	async checkEmail(email: string): Promise<boolean> {
+		// Simulate API call to check email availability
+		await new Promise((resolve) => setTimeout(resolve, 500));
+
+		if (localStorage.getItem(email) === email) {
+			return false;
+		}
+		return true;
+	}
+
+	async register(email: string, username: string): Promise<void> {
+		localStorage.setItem(username, username);
+		localStorage.setItem(email, email);
+		// Simulate API call
+		await new Promise((resolve) => setTimeout(resolve, 1500));
+
+		this.email = email;
+		this.username = username;
+		this.name = username; // Can be updated later
+		this.isAuthenticated = true;
+	}
 }
 
 export const user = new UserState();
